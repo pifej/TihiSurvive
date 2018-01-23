2231f35b1cacfa84b57a066bed963b6674dc83f3
root <root@peter-Inspiron-N5110>
2018-01-23 21:53:33 +0100

    Access Tihi's HP increaser and decreaser (facade dp)

---
+++ src/Karma.java
@@ -0,0 +1,19 @@
+/**
+ * @author root
+ *
+ */
+public class Karma {
+	private Tihi tihi;
+	
+	public Karma() {
+		tihi = new Tihi();
+	}
+	
+	void addHP(int add) {
+		tihi.addHP(add);
+	}
+	
+	void subHP(int sub) {
+		tihi.subHP(sub);
+	}
+}

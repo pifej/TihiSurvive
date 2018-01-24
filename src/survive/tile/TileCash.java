79cbb93521ae4c942583034918ca7ac8b7bd1127
root <root@peter-Inspiron-N5110>
2018-01-24 22:10:56 +0100

    - Class to store instances of tile type classes

--- src/survive/tile/TileCash.java
+++ src/survive/tile/TileCash.java
@@ -13,13 +13,37 @@ import java.util.Hashtable;
 public class TileCash {
 	private static Hashtable<String, Tile> tileMap = new Hashtable<String, Tile>();
 	
+	/**
+	 * Gets the clone of a given class
+	 * @param type type of the class we want to clone
+	 * @return	the cloned class
+	 */
 	public static Tile getTile(String type) {
 		Tile cachedTile = tileMap.get(type);
 		return (Tile)cachedTile.clone();
 	}
 	
+	/**
+	 * Loads the instance of the subclasses into a hash table
+	 */
 	public static void loadCache() {
+		Food food = new Food();
+		tileMap.put(food.type, food);
 		
+		Regular regular = new Regular();
+		tileMap.put(regular.type, regular);
+		
+		Obstacle obstacle = new Obstacle();
+		tileMap.put(obstacle.type, obstacle);
+		
+		Water water = new Water();
+		tileMap.put(water.type, water);
+		
+		Rest rest = new Rest();
+		tileMap.put(rest.type, rest);
+		
+		Paradise paradise = new Paradise();
+		tileMap.put(paradise.type, paradise);
 	}
 
 }
